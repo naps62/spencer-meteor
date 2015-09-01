b4ba8d65d155139a3445cf06ad067e99b26d13fe
Miguel Palhas <mpalhas@gmail.com>
2015-09-01 01:28:37 +0100

    WIP [ci skip]

--- client/views/expenses/expensesNew.js
+++ client/views/expenses/expensesNew.js
@@ -1,7 +1,27 @@
 AutoForm.hooks({
-  'newExpenseForm': {
+  newExpenseForm: {
     onSuccess: function() {
       Router.go('/');
+    },
+    formToDoc: function(doc) {
+      if (doc.tags) {
+        doc.tags = doc.tags.split(',').map(function(tag) {
+          return tag.trim();
+        });
+      }
+      console.log(doc.tags);
+
+      return doc;
+    },
+    docToForm: function(doc) {
+      doc.tags = doc.tags.join(', ');
+      return doc;
     }
+  }
+});
+
+Template.expensesNew.helpers({
+  expenseSchema: function() {
+    return NewExpenseSchema;
   }
 });
